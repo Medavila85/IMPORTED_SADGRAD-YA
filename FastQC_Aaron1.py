4261c5bd7c67feb6783b6024977161d06a86d95f
SadGrad-ya <79035609+SadGrad-ya@users.noreply.github.com>
2021-07-16 15:36:16 -0600

    Update FastQC_Aaron1.py

--- FastQC_Aaron1.py
+++ FastQC_Aaron1.py
@@ -10,16 +10,31 @@ import subprocess
 import sys
 import argparse
 
+FastQC ='/usr/local/FastQC/fastqc'
 
-print("Enter outfile name")
-directoryanme = input()
-outdir = os.mkdir(directoryanme)
-print("Enter location of FastQC")
-FastQC= [] 
-FastQC.append(input())
-print("Enter input file location")
-FastQC.append(input())
-print("Enter outfile name")
-FastQC.append('-o') 
-FastQC.append(directoryanme) 
-subprocess.call(FastQC)
+
+
+
+parser = argparse.ArgumentParser(description='Parameters')
+parser.add_argument('-i', '--input_directory for FQ', metavar="DIRNAME", dest='raw', required=True, help='Required: Input directory with raw fastq files.')
+parser.add_argument('-o', '--output_dir', metavar="DIRNAME", dest='outdir', required=True, help='Required: Name of output directory to create.')
+args = parser.parse_args()
+
+raw = args.raw
+outdir = args.outdir
+
+if os.path.exists(outdir):
+      print("Error: Output directory already exists.  Please choose different directory or remove old directory.")
+      quit()
+os.mkdir(outdir)
+os.mkdir('%s/FQ_output'%outdir)
+
+fastq_log = open('%s/fastq_log.txt'%outdir,'a')
+
+comm = '%s %s -o %s/FQ_output'%(FastQC, raw, outdir)
+process = subprocess.Popen(comm, stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+process.wait()
+fastq_log.write('\n'.join([x.decode('utf-8').rstrip('\n') for x in iter(process.stdout.readlines())]))
+fastq_log.write('\n'.join([x.decode('utf-8').rstrip('\n') for x in iter(process.stderr.readlines())]))
+fastq_log.write('bwa command: %s\n'%comm)
+print("completed")
